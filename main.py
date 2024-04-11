f0978fe17f83c1eb9a4edbcff041efb1278f34a1
HellsegaMosken <402422100@qq.com>
2024-04-11 17:31:39 +0800

    2024.4.11

---
+++ main.py
@@ -0,0 +1,34 @@
+"""
+作者：吕枫烨
+创建时间：2024/4/11 17:25
+项目：Python-Core-50-Courses
+包：
+标题：
+描述：
+"""
+
+# 修改包下载的源为国内的源
+
+pip config set global.index-url https://pypi.doubanio.com/simple
+
+# 更好的交互环境
+
+pip install ipython
+pip unstall ipython
+
+# 变量类型
+
+type()
+int()
+float()
+str()
+chr()
+ord()
+
+
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,31 @@
+"""
+作者：吕枫烨
+创建时间：2024/4/11 17:25
+项目：Python-Core-50-Courses
+包：
+标题：
+描述：
+"""
+
+print('hello, world')
+
+a = 45          # 变量a保存了45
+b = 12          # 变量b保存了12
+print(a + b)    # 57
+print(a - b)    # 33
+print(a * b)    # 540
+print(a / b)    # 3.75
+
+a = 100
+b = 12.345
+c = 'hello, world'
+d = True
+print(type(a))    # <class 'int'>
+print(type(b))    # <class 'float'>
+print(type(c))    # <class 'str'>
+print(type(d))    # <class 'bool'>
+
+
+
+
+
