79ce06be78c90f39c5b837311820bada9020715d
HellsegaMosken <402422100@qq.com>
2024-04-16 17:32:53 +0800

    2024.4.16

--- 1.test.py
+++ 1.test.py
@@ -1217,7 +1217,49 @@ print(calc(a=1, b=2, c=3))     # 6
 print(calc(1, 2, c=3, d=4))    # 10
 #endregion
 
+# region    # ------------ 函数：装饰器 ------------
+import random
+import time
 
+def download(filename):
+    print(f'开始下载{filename}.')
+    time.sleep(random.randint(2, 6))
+    print(f'{filename}下载完成.')
+
+def upload(filename):
+    print(f'开始上传{filename}.')
+    time.sleep(random.randint(4, 8))
+    print(f'{filename}上传完成.')
+
+download('MySQL从删库到跑路.avi')
+upload('Python从入门到住院.pdf')
+
+import time
+# 定义装饰器函数，它的参数是被装饰的函数或类
+def record_time(func):
+    # 定义一个带装饰功能（记录被装饰函数的执行时间）的函数
+    # 因为不知道被装饰的函数有怎样的参数所以使用*args和**kwargs接收所有参数
+    # 在Python中函数可以嵌套的定义（函数中可以再定义函数）
+    def wrapper(*args, **kwargs):
+        # 在执行被装饰的函数之前记录开始时间
+        start = time.time()
+        # 执行被装饰的函数并获取返回值
+        result = func(*args, **kwargs)
+        # 在执行被装饰的函数之后记录结束时间
+        end = time.time()
+        # 计算和显示被装饰函数的执行时间
+        print(f'{func.__name__}执行时间: {end - start:.3f}秒')
+        # 返回被装饰函数的返回值（装饰器通常不会改变被装饰函数的执行结果）
+        return result
+
+    # 返回带装饰功能的wrapper函数
+    return wrapper
+
+download = record_time(download)
+upload = record_time(upload)
+download('MySQL从删库到跑路.avi')
+upload('Python从入门到住院.pdf')
+#endregion
 
 
 
