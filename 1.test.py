3ae70780fb148a9643175c4a6e8094efd061e75e
HellsegaMosken <402422100@qq.com>
2024-04-26 18:02:19 +0800

    2024.4.26

--- 1.test.py
+++ 1.test.py
@@ -1347,13 +1347,170 @@ def fib(n):
         return 1
     return fib(n - 1) + fib(n - 2)
 
-
 # 打印前20个斐波那契数
+for i in range(1, 21):
+    print(fib(i))
+
+### 循环递推
+def fib(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+        # print(a, b)
+    return a
+
 for i in range(1, 21):
     print(fib(i))
 #endregion
 
+# region    # ------------ 定义类 ------------
+class Student:
 
+    def study(self, course_name):
+        print(f'学生正在学习{course_name}.')
+
+    def play(self):
+        print(f'学生正在玩游戏.')
+
+### 创建和使用对象
+stu1 = Student()
+stu2 = Student()
+print(stu1)    # <__main__.Student object at 0x10ad5ac50>
+print(stu2)    # <__main__.Student object at 0x10ad5acd0>
+print(hex(id(stu1)), hex(id(stu2)))    # 0x10ad5ac50 0x10ad5acd0
+
+# 通过“类.方法”调用方法，第一个参数是接收消息的对象，第二个参数是学习的课程名称
+Student.study(stu1, 'Python程序设计')    # 学生正在学习Python程序设计.
+# 通过“对象.方法”调用方法，点前面的对象就是接收消息的对象，只需要传入第二个参数
+stu1.study('Python程序设计')             # 学生正在学习Python程序设计.
+
+Student.play(stu2)    # 学生正在玩游戏.
+stu2.play()           # 学生正在玩游戏.
+#endregion
+
+# region    # ------------ 初始化方法 ------------
+class Student:
+    """学生"""
+
+    def __init__(self, name, age):
+        """初始化方法"""
+        self.name = name
+        self.age = age
+
+    def study(self, course_name):
+        """学习"""
+        print(f'{self.name}正在学习{course_name}.')
+
+    def play(self):
+        """玩耍"""
+        print(f'{self.name}正在玩游戏.')
+
+# 由于初始化方法除了self之外还有两个参数
+# 所以调用Student类的构造器创建对象时要传入这两个参数
+stu1 = Student('骆昊', 40)
+stu2 = Student('王大锤', 15)
+stu1.study('Python程序设计')    # 骆昊正在学习Python程序设计.
+stu2.play()                    # 王大锤正在玩游戏.
+#endregion
+
+# region    # ------------ 打印对象 ------------
+class Student:
+    """学生"""
+
+    def __init__(self, name, age):
+        """初始化方法"""
+        self.name = name
+        self.age = age
+
+    def study(self, course_name):
+        """学习"""
+        print(f'{self.name}正在学习{course_name}.')
+
+    def play(self):
+        """玩耍"""
+        print(f'{self.name}正在玩游戏.')
+
+    def __repr__(self):
+        return f'{self.name}: {self.age}'
+
+stu1 = Student('骆昊', 40)
+print(stu1)  # 骆昊: 40
+students = [stu1, Student('李元芳', 36), Student('王大锤', 25)]
+print(students)  # [骆昊: 40, 李元芳: 36, 王大锤: 25]
+#endregion
+
+# region    案例：定义一个类描述数字时钟。
+import time
+
+# 定义数字时钟类
+class Clock(object):
+    """数字时钟"""
+
+    def __init__(self, hour=0, minute=0, second=0):
+        """初始化方法
+        :param hour: 时
+        :param minute: 分
+        :param second: 秒
+        """
+        self.hour = hour
+        self.min = minute
+        self.sec = second
+
+    def run(self):
+        """走字"""
+        self.sec += 1
+        if self.sec == 60:
+            self.sec = 0
+            self.min += 1
+            if self.min == 60:
+                self.min = 0
+                self.hour += 1
+                if self.hour == 24:
+                    self.hour = 0
+
+    def show(self):
+        """显示时间"""
+        return f'{self.hour:0>2d}:{self.min:0>2d}:{self.sec:0>2d}'
+
+
+# 创建时钟对象
+clock = Clock(23, 59, 58)
+while True:
+    # 给时钟对象发消息读取时间
+    print(clock.show())
+    # 休眠1秒钟
+    time.sleep(1)
+    # 给时钟对象发消息使其走字
+    clock.run()
+#endregion
+
+# region    案例：定义一个类描述平面上的点，要求提供计算到另一个点距离的方法。
+class Point(object):
+    """屏面上的点"""
+
+    def __init__(self, x=0, y=0):
+        """初始化方法
+        :param x: 横坐标
+        :param y: 纵坐标
+        """
+        self.x, self.y = x, y
+
+    def distance_to(self, other):
+        """计算与另一个点的距离
+        :param other: 另一个点
+        """
+        dx = self.x - other.x
+        dy = self.y - other.y
+        return (dx * dx + dy * dy) ** 0.5
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+
+p1 = Point(3, 5)
+p2 = Point(6, 9)
+print(p1, p2)
+print(p1.distance_to(p2))
+#endregion
 
 
 
