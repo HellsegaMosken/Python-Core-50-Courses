4f2aed0f13e30ba6a8fba96ead92820884b0468f
HellsegaMosken <402422100@qq.com>
2024-05-09 17:26:48 +0800

    2024.5.9

--- 1.test.py
+++ 1.test.py
@@ -1982,13 +1982,109 @@ uuid.uuid1().hex
 uuid.uuid1().hex
 #endregion
 
+# region    # ------------ 文件读取 ------------
+### 整个文件读取
+file = open('致橡树.txt', 'r', encoding='utf-8')
+print(file.read())
+file.close()
 
+### 按行读取
+file = open('致橡树.txt', 'r', encoding='utf-8')
+for line in file:
+    print(line, end='')
+file.close()
 
+file = open('致橡树.txt', 'r', encoding='utf-8')
+lines = file.readlines()
+for line in lines:
+    print(line, end='')
+file.close()
 
+### 追加写入
+file = open('致橡树.txt', 'a', encoding='utf-8')
+file.write('\n标题：《致橡树》')
+file.write('\n作者：舒婷')
+file.write('\n时间：1977年3月')
+file.close()
+#endregion
 
+# region    # ------------ 异常处理机制 ------------
+file = None
+try:
+    file = open('致橡树.txt', 'r', encoding='utf-8')
+    print(file.read())
+except FileNotFoundError:
+    print('无法打开指定的文件!')
+except LookupError:
+    print('指定了未知的编码!')
+except UnicodeDecodeError:
+    print('读取文件时解码错误!')
+finally:
+    if file:
+        file.close()
 
+### 案例：调用求阶乘的函数fac，通过try...except...结构捕获输入错误的异常并打印异常对象（显示异常信息），如果输入正确就计算阶乘并结束程序。
+class InputError(ValueError):
+    """自定义异常类型"""
+    pass
 
+def fac(num):
+    """求阶乘"""
+    if num < 0:
+        raise InputError('只能计算非负整数的阶乘')
+    if num in (0, 1):
+        return 1
+    return num * fac(num - 1)
 
+flag = True
+while flag:
+    num = int(input('n = '))
+    try:
+        print(f'{num}! = {fac(num)}')
+        flag = False
+    except InputError as err:
+        print(err)
+
+#endregion
+
+# region    # ------------ 上下文语法：读文件 ------------
+try:
+    with open('致橡树.txt', 'r', encoding='utf-8') as file:
+        print(file.read())
+except FileNotFoundError:
+    print('无法打开指定的文件!')
+except LookupError:
+    print('指定了未知的编码!')
+except UnicodeDecodeError:
+    print('读取文件时解码错误!')
+#endregion
+
+# region    # ------------ 读写二进制文件 ------------
+try:
+    with open('guido.jpg', 'rb') as file1:
+        data = file1.read()
+    with open('吉多.jpg', 'wb') as file2:
+        file2.write(data)
+except FileNotFoundError:
+    print('指定的文件无法打开.')
+except IOError:
+    print('读写文件时出现错误.')
+print('程序执行结束.')
+#endregion
+
+# region    # ------------ 读写文件：指定size以节省内存开销 ------------
+try:
+    with open('guido.jpg', 'rb') as file1, open('吉多.jpg', 'wb') as file2:
+        data = file1.read(512)
+        while data:
+            file2.write(data)
+            data = file1.read()
+except FileNotFoundError:
+    print('指定的文件无法打开.')
+except IOError:
+    print('读写文件时出现错误.')
+print('程序执行结束.')
+#endregion
 
 
 
